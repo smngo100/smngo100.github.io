a1eb84c7ed3389e54a79d784072be5aa985cf4f2
smngo100 <smngo100@gmail.com>
2025-06-20 11:13:57 -0700

    Add files via upload

---
+++ iphone-evolution/phone.js
@@ -0,0 +1,311 @@
+const iphoneData = {
+  'The Original': 
+  [
+    {
+    model: 'iPhone',
+    releaseDate: 'June 29, 2007',
+    quote: 'Apple reinvents the phone.',
+    image: 'iphone-images/the_og/the_original.png',
+    }
+  ],
+  "iPhone 3 Series": 
+  [
+    {
+      model: 'iPhone 3G',
+      releaseDate: 'July 11, 2008',
+      quote: '"The iPhone you have been waiting for."',
+      image: 'iphone-images/series_03/iphone_3g.png',
+    },
+    {
+      model: 'iPhone 3GS',
+      releaseDate: 'June 19, 2009',
+      quote: '"The fastest, most powerful iPhone yet."',
+      image: 'iphone-images/series_03/iphone_3gs.png',
+    }
+  ],
+"iPhone 4 Series": 
+  [
+    {
+      model: 'iPhone 4',
+      releaseDate: 'June 24, 2010',
+      quote: '"This changes everything. Again."',
+      image: 'iphone-images/series_04/iphone_4.png',
+    },
+    {
+      model: 'iPhone 4S',
+      releaseDate: 'Oct. 14, 2011',
+      quote: '"The most amazing iPhone yet."',
+      image: 'iphone-images/series_04/iphone_4.png',
+    }
+  ],
+"iPhone 5 Series": 
+  [
+    {
+      model: 'iPhone 5',
+      releaseDate: 'Sept. 21, 2012',
+      quote: '"The biggest thing to happen to iPhone since iPhone."',
+      image: 'iphone-images/series_05/iphone_5.png',
+    },
+    {
+      model: 'iPhone 5S',
+      releaseDate: 'Sept. 20, 2013',
+      quote: '"You\'re more powerful than you think."',
+      image: 'iphone-images/series_05/iphone_5s.png',
+    },
+    {
+      model: 'iPhone 5C',
+      releaseDate: 'Sept. 20, 2013',
+      quote: '"For the colorful."',
+      image: 'iphone-images/series_05/iphone_5c.png',
+    }
+  ],
+"iPhone 6 Series": 
+  [
+    {
+      model: 'iPhone 6 & 6 Plus',
+      releaseDate: 'Sept. 19, 2014',
+      quote: '"Bigger than bigger."',
+      image: 'iphone-images/series_06/iphone_6_6plus.png',
+    },
+    {
+      model: 'iPhone iPhone 6s & 6s Plus',
+      releaseDate: 'Sept. 19, 2015',
+      quote: '"The only thing that\'s changed is everything."',
+      image: 'iphone-images/series_06/iphone_6s_6splus.png',
+    }
+  ],
+"iPhone 7 Series":
+  [
+    {
+      model: 'iPhone 7',
+      releaseDate: 'Sept. 16, 2016',
+      quote: '"This is 7."',
+      image: 'iphone-images/series_07/iphone_7.png',
+    },
+    {
+      model: 'iPhone 7 Plus',
+      releaseDate: 'Sept. 16, 2016',
+      quote: '"This is 7."',
+      image: 'iphone-images/series_07/iphone_7plus.png',
+    }
+  ],
+"iPhone 8 Series":
+  [
+    {
+      model: 'iPhone 8',
+      releaseDate: 'Sept. 22, 2017',
+      quote: '"A new generation of iPhone."',
+      image: 'iphone-images/series_08/iphone_8.png',
+    },
+    {
+      model: 'iPhone 8 Plus',
+      releaseDate: 'Sept. 22, 2017',
+      quote: '"A new generation of iPhone."',
+      image: 'iphone-images/series_08/iphone_8plus.png',
+    }
+  ],
+"iPhone X Series":
+  [
+    {
+      model: 'iPhone X',
+      releaseDate: 'Nov. 3, 2017',
+      quote: '"Say hello to the future."',
+      image: 'iphone-images/series_x/iphone_x.png',
+    },
+    {
+      model: 'iPhone XS & XS Max',
+      releaseDate: 'Sept. 21, 2018',
+      quote: '"Welcome to the big screens."',
+      image: 'iphone-images/series_x/iphone_xs_xsmax.png',
+    },
+    {
+      model: 'iPhone XR',
+      releaseDate: 'Oct. 26, 2018',
+      quote: '"Brilliant. In every way."',
+      image: 'iphone-images/series_x/iphone_xr.png',
+    }
+  ],
+"iPhone 11 Series":
+  [
+    {
+      model: 'iPhone 11',
+      releaseDate: 'Sept. 20, 2019',
+      quote: '"Just the right amount of everything."',
+      image: 'iphone-images/series_11/iphone_11.png',
+    },
+    {
+      model: 'iPhone 11 Pro & Pro Max',
+      releaseDate: 'Sept. 20, 2019',
+      quote: '"Pro cameras. Pro display. Pro performance."',
+      image: 'iphone-images/series_11/iphone_11pro_11promax.png',
+    }
+  ],
+"iPhone 12 Series": 
+  [
+    {
+      model: 'iPhone 12',
+      releaseDate: 'Oct. 23, 2020',
+      quote: '"Blast past fast."',
+      image: 'iphone-images/series_12/iphone_12.png',
+    },
+    {
+      model: 'iPhone 12 mini',
+      releaseDate: 'Nov. 13, 2020',
+      quote: '"Blast past fast."',
+      image: 'iphone-images/series_12/iphone_12mini.png',
+    },
+    {
+      model: 'iPhone 12 Pro & Pro Max',
+      releaseDate: 'Oct. 23, 2020',
+      quote: '"It\'s a leap year."',
+      image: 'iphone-images/series_12/iphone_12pro_12promax.png',
+    }
+  ],
+"iPhone 13 Series": 
+  [
+    {
+      model: 'iPhone 13',
+      releaseDate: 'Sept. 24, 2021',
+      quote: '"Your new superpower."',
+      image: 'iphone-images/series_13/iphone_13.png',
+    },
+    {
+      model: 'iPhone 13 mini',
+      releaseDate: 'Sept. 24, 2021',
+      quote: '"Your new superpower."',
+      image: 'iphone-images/series_13/iphone_13mini.png',
+    },
+    {
+      model: 'iPhone 13 Pro & Pro Max',
+      releaseDate: 'Sept. 24, 2021',
+      quote: '"Oh. So. Pro."',
+      image: 'iphone-images/series_13/iphone_13pro_13promax.png',
+    }
+  ],
+"iPhone 14 Series": 
+  [
+    {
+      model: 'iPhone 14 & 14 Plus',
+      releaseDate: 'Sept. 16, 2022',
+      quote: '"Big and Bigger."',
+      image: 'iphone-images/series_14/iphone_14_14plus.png',
+    },
+    {
+      model: 'iPhone 14 Pro & Pro Max',
+      releaseDate: 'Sept. 16, 2022',
+      quote: '"Pro. Beyond."',
+      image: 'iphone-images/series_14/iphone_14pro_14promax.png',
+    },
+  ],
+"iPhone 15 Series": 
+  [
+    {
+      model: 'iPhone 15 & 15 Plus',
+      releaseDate: 'Sept. 12, 2023',
+      quote: '"Newphoria."',
+      image: 'iphone-images/series_15/iphone_15_15plus.png',
+    },
+    {
+      model: 'iPhone 15 Pro & Pro Max',
+      releaseDate: 'Sept. 12, 2023',
+      quote: '"Titanium. So string. So light. So Pro."',
+      image: 'iphone-images/series_15/iphone_15pro_15promax.png',
+    },
+  ],
+"iPhone 16 Series": 
+  [
+    {
+      model: 'iPhone 16 & 16 Plus',
+      releaseDate: 'Sept. 20, 2024',
+      quote: '"Hello, Apple Intelligence."',
+      image: 'iphone-images/series_16/iphone_16_16plus.png',
+    },
+    {
+      model: 'iPhone 16 Pro & Pro Max',
+      releaseDate: 'Sept. 20, 2024',
+      quote: '"Built for Apple Intelligence."',
+      image: 'iphone-images/series_16/iphone_16pro_16promax.png',
+    },
+    {
+      model: 'iPhone 16e',
+      releaseDate: 'Feb. 28, 2025',
+      quote: '"Latest iPhone."',
+      image: 'iphone-images/series_16/iphone_16e.png',
+    }
+  ],
+"iPhone SE Series": 
+  [
+    {
+      model: 'iPhone SE',
+      releaseDate: 'March 31, 2016',
+      quote: '"A big step for small."',
+      image: 'iphone-images/series_se/iphone_se.png',
+    },
+    {
+      model: 'iPhone SE 2nd Generation',
+      releaseDate: 'April 24, 2020',
+      quote: '"Lots to love. Less to spend."',
+      image: 'iphone-images/series_se/iphone_se_2ndgen.png',
+    },
+    {
+      model: 'iPhone SE 3rd Generation',
+      releaseDate: 'March 18, 2022',
+      quote: '"Love the power. Love the price."',
+      image: 'iphone-images/series_se/iphone_se_3rdgen.png',
+    },
+  ],
+};
+
+
+let iphoneHTML = '';
+
+Object.entries(iphoneData).forEach(([series, phonesArray]) => {
+  
+  let html = `
+    <div class="series-title">${series}</div>
+    <div class="devices-container">
+    `;
+  
+  phonesArray.forEach(iphone => {
+    const { model, releaseDate, quote, image } = iphone;
+  
+    /*
+    I removed the model container because it seemed unnecessary 
+    html += `
+      <div class="multiple-devices-container">
+        <div class="series-container">
+          <div class="model-container">
+            <div class="model-image-container">
+              <img class="model-image" src="${image}">
+            </div>
+            <div class="model-details">
+              <div class="iphone-model">${model}</div>
+              <div class="release-date">${releaseDate}</div>
+              <div class="quote">${quote}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+    */
+
+    html += `
+      <div class="series-container">
+        <div class="model-image-container">
+          <img class="model-image" src="${image}">
+        </div>
+        <div class="model-details">
+          <div class="iphone-model">${model}</div>
+          <div class="release-date">${releaseDate}</div>
+          <div class="quote">${quote}</div>
+        </div>
+      </div>
+    `;
+  });
+
+  html += `</div>`;
+  
+  iphoneHTML += html;
+});
+
+document.querySelector('.js-models-grid').innerHTML = iphoneHTML;
